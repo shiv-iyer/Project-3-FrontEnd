d653c0d7b9ab2b1c1b38e9985386859ee4742263
Shiv <shiviyer55x@gmail.com>
2023-05-16 14:53:19 +0800

    two way binding for login form done

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,42 @@
 import React, {useState} from "react";
 
 export default function Login() {
+
+    // massive state by wrapping an object within useState
+    const [formState, setFormState] = useState({
+        // initialize as empty string
+        email: "",
+        password: ""
+    })
+
+    const updateFormField = (event) => {
+        setFormState({
+            // when you spread formState, it will always take the last value, even if repeat keys
+            ...formState,
+            [event.target.name]: event.target.value
+         })
+    }
+
     return (
         <React.Fragment>
             <h1>Login</h1>
             <div>
                 <label>Email</label>
-                <input type="text"/>
+                <input
+                    type="text"
+                    name="email"
+                    value={formState.email}
+                    onChange={updateFormField}
+                    />
             </div>
             <div>
                 <label>Password</label>
-                <input type="password"/>
+                <input
+                 type="password"
+                 name="password"
+                 value={formState.password}
+                 onChange={updateFormField}
+                 />
             </div>
         </React.Fragment>
     )
