f008d982a115f9655ef9a83d8f1194608f858811
Shiv <shiviyer55x@gmail.com>
2023-05-17 13:40:52 +0800

    really bad styling to start

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -44,58 +44,64 @@ export default function Register () {
         <React.Fragment>
             <h1>Register</h1>
             <div className="form-group">
-                <div>
+                <div className="form-item my-2">
                     <label>First Name</label>
                     <input
                         type="text"
                         name="first_name"
                         value={formState.first_name}
                         onChange={updateFormField}
+                        className="ms-2"
                         />
                 </div>
-                <div>
+                <div className="form-item my-2">
                     <label>Last Name</label>
                     <input
                     type="text"
                     name="last_name"
                     value={formState.last_name}
                     onChange={updateFormField}
+                    className="ms-2"
                     />
                 </div>
-                <div>
+                <div className="form-item my-2">
                     <label>Username</label>
                     <input
                         type="text"
                         name="username"
                         value={formState.username}
                         onChange={updateFormField}
+                        className="ms-2"
                         />
                 </div>
-                <div>
+                <div className="form-item my-2">
                     <label>Password</label>
                     <input
                         type="password"
                         name="password"
                         value={formState.password}
                         onChange={updateFormField}
+                        className="ms-2"
                         />
                 </div>
-                <div>
+                <div className="form-item my-2">
                     <label>Email</label>
                     <input
                         type="text"
                         name="email"
                         value={formState.email}
                         onChange={updateFormField}
+                        className="ms-2"
                         />
                 </div>
-                <div>
+                <div className="form-item my-2">
                     <label>Contact Number</label>
                     <input
                         type="text"
                         name="contact_number"
                         value={formState.contact_number}
                         onChange={updateFormField}
+                        className="ms-2"
                         />
                 </div>
             </div>
