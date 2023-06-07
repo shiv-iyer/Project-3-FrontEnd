adf6f3094ac4e08ef0fb28c680db3a03f98b7bfa
Shiv <shiviyer55x@gmail.com>
2023-06-07 14:07:13 +0800

    added some basics to the home page; hero image, text, minimal styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,21 @@
 // homepage
 
+// really cool wallpapers for reference: https://wallpapercave.com/pokemon-mystery-dungeon-wallpaper
+
+import { Typography } from '@mui/material';
 import React from 'react';
+import {Container} from 'react-bootstrap';
 
 export default function Home() {
     return (
         <React.Fragment>
-            <h1>Home</h1>
-            <h2>work in progress</h2>
+            <Container>
+                <div className="welcome-container">
+                    <Typography variant="h2" className="header-text">
+                        Welcome to PokéPort!
+                    </Typography>
+                </div>
+            </Container>
         </React.Fragment>
     )
 }
